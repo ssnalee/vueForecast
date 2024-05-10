d5a08c383844e7a96baff364ad976339024ace75
ssna lee <97poolbbang@naver.com>
2024-05-10 18:48:05 +0900

    re

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
 
 })
 
-module.exports = {
-  publicPath: '/vueForecast',
-  outputDir: 'docs'
-}
+// module.exports = {
+//   publicPath: '/vueForecast',
+//   outputDir: 'docs'
+// }
